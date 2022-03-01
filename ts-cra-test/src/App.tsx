ba2df71abecad46aa5d22f469050b275c11b0989
NANGGI <mqsora@naver.com>
2022-03-02 00:47:48 +0900

    Add files via upload

--- ts-cra-test/src/App.tsx
+++ ts-cra-test/src/App.tsx
@@ -1,34 +1,58 @@
-import React, { ReactElement, ReactNode } from 'react';
+import React, { FC, ReactElement, ReactNode, useState } from 'react';
 import './App.css';
 
+/*
+const HeadingFC: FC<{ title: string }> = ({ title }) => {
+  return (
+    <h1>{title}</h1>
+  )
+}
+*/
+
 // Conventional props
-function Heading({ title }: { title: string; }) {
-  return <h1>{title}</h1>;
+function Heading({ title }: { title: string }): ReactElement | null {
+  return (
+    <h1>{title}</h1>
+  )
 }
 
-function HeadingWithContent({ children }: { children: ReactNode }): ReactElement {
-  return <h1>{children}</h1>;
+function HeadingWithContent({ children }: { children: ReactNode }): ReactElement | null {
+  return (
+    <h1>{children}</h1>
+  )  
 }
 
 // defaultProps
 const defaultContainerProps = {
   heading: <strong>My Heading</strong>
-};
+}
+function Container({ heading, children }: { heading: ReactNode, children: ReactNode } & typeof defaultContainerProps): ReactElement | null {
+  return (
+    <div>
+      <h1>{heading}</h1>
+      {children}
+    </div>
+  )
+}
+Container.defaultProps = defaultContainerProps;
 
-function Container({ heading, children }: { heading: ReactNode, children: ReactNode }): ReactElement {
-  return <h1>{children}</h1>;
+// Functional Props
+function TextWithNumber({
+  children
+}: {
+  children: (num: string) => ReactNode
+}) {
+  const [state, setState] = useState<number | null>();
 }
 
 function App() {
   return (
     <div>
-      <Heading title="Hello"></Heading>
+      <Heading title="Hello there"></Heading>
       <HeadingWithContent>
-        <strong>hi!</strong>
+        <strong>Hi !</strong>
       </HeadingWithContent>
-      <Container>
-        Foo
-      </Container>
+      {/* <TextWithNumber>{(num: number) => <div>Today's number num</div>}</TextWithNumber> */}
     </div>
   );
 }
